72da2d43c51f58f9c085b27b623d16eef106ce6f
presedo <presedo@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-30 13:19:39 +0000

    initial release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/facebook/trunk@32785 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "facebook"
+#
+# Auto generated 20-01-2010 12:10
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Framework for Facebook applications',
+	'description' => 'A simple framework that embeds the Facebook libraries and a plugin to develop callbacks for Facebook applications.',
+	'category' => 'services',
+	'author' => 'Olivier Schopfer & Roberto Presedo',
+	'author_email' => 'ops@wcc-coe.org ; rpresedo@cobweb.ch',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.2.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"432b";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"d97d";s:17:"ext_localconf.php";s:4:"6672";s:14:"ext_tables.php";s:4:"5e1b";s:12:"flexform.xml";s:4:"19f3";s:16:"locallang_db.xml";s:4:"7503";s:19:"doc/wizard_form.dat";s:4:"4ef2";s:20:"doc/wizard_form.html";s:4:"13dc";s:19:"fb_lib/facebook.php";s:4:"7c32";s:27:"fb_lib/facebook_desktop.php";s:4:"c7a4";s:35:"fb_lib/facebookapi_php5_restlib.php";s:4:"62eb";s:34:"fb_lib/jsonwrapper/jsonwrapper.php";s:4:"d3b3";s:40:"fb_lib/jsonwrapper/jsonwrapper_inner.php";s:4:"2bad";s:32:"fb_lib/jsonwrapper/JSON/JSON.php";s:4:"3ce5";s:31:"fb_lib/jsonwrapper/JSON/LICENSE";s:4:"f572";s:29:"pi1/class.tx_facebook_pi1.php";s:4:"984b";s:17:"pi1/locallang.xml";s:4:"be75";s:29:"static/facebook/constants.txt";s:4:"dd09";s:25:"static/facebook/setup.txt";s:4:"936b";}',
+);
+
+?>
